85aedd3d3a53071aec33370caad26cdc49231d99
u2 <zhangyaning1985@gmail.com>
2016-08-23 21:17:55 +0800

    fixed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.autolink();
+  deployer.deploy(Resolver);
+  deployer.deploy(EtherRouter);
   deployer.deploy(TheAnswer);
   deployer.deploy(Multiplier);
   deployer.deploy(Lost);
